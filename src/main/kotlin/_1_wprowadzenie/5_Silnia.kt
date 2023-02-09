86d4b026f5c1bca48cdd2a5bdf2129ff7e478d7a
pkwiat <pawel.kwiatkowski2000@gmail.com>
2023-02-09 08:28:07 +0100

    Nowe zmiany

--- src/main/kotlin/_1_wprowadzenie/5_Silnia.kt
+++ src/main/kotlin/_1_wprowadzenie/5_Silnia.kt
@@ -5,5 +5,13 @@ fun main(args: Array<String>) {
     // Silnia - n! - 1*2*3*...*n
     print("Podaj liczbe nieujemną: ")
 
-    val input - readLine()`
+    // Int i Int?
+    // String i String?
+
+    /*var nieNulowe: Int = null? Jeżeli daję znak zapytania na końcu to mówię - "to może być null"*/
+
+    //!! -> NA STO PROCENT TO NIE JEST NULL
+    //? -> TO MOŻE BYĆ NULL
+
+    val input = readLine()!!.toInt()
 }
