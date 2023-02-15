b01be4d2568c0b32551ec6ca452739d2f37fe013
pkwiat <pawel.kwiatkowski2000@gmail.com>
2023-02-15 13:00:07 +0100

    15.02.2023

--- src/main/kotlin/_3_Zaawansowane_aspekty_Kotlina/8_Delegacje.kt
+++ src/main/kotlin/_3_Zaawansowane_aspekty_Kotlina/8_Delegacje.kt
@@ -1,6 +1,31 @@
 package _3_Zaawansowane_aspekty_Kotlina
 
+interface Base{
+    fun print()
+}
+
+class BaseImpl(private val x: Int): Base{
+    override fun print() {
+        print(x)
+    }
+
+}
+
+class BaseImpl2(b: Base): Base by b
+
 fun main() {
-    
+
+    val baseImpl = BaseImpl(10)
+    val baseImpl2 = BaseImpl2(baseImpl)
+    baseImpl2.print()
+
+    // Lazy pozwala odwlec moment obliczenia wartości do pierwszego pojawienia się zmiennej
+
+
+    val x by lazy {
+        10
+    }
+
+    println(x)
 
 }
---
+++ src/main/kotlin/_3_Zaawansowane_aspekty_Kotlina/9_Debugger.kt
@@ -0,0 +1,21 @@
+package _3_Zaawansowane_aspekty_Kotlina
+
+fun main() {
+
+    val x = 10
+    val y = 20
+
+    println(f1())
+
+}
+
+fun f1(): String {
+    val z = "Hello"
+    return f2()
+}
+
+fun f2(): String {
+    val t = -1f
+    var v = 'A'
+    return "World"
+}
